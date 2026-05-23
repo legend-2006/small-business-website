14eb317ed112b1067d3f5dfc6179c9cff308d25b
legend <moengkarabo607@gmail.com>
2026-05-23 23:43:17 +0200

    fix netlify deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import react from "@vitejs/plugin-react";
+import netlify from "@netlify/vite-plugin-tanstack-start";
 import { defineConfig } from "vite";
 import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [
     tanstackStart(),
+    netlify(),
     tsConfigPaths(),
     tailwindcss(),
     react(),
